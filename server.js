5abe17211052dc025133ec53d518c98301671c5d
daniii-j <dani.jin@generac.com>
2023-01-19 23:55:00 -0800

    hi

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express(); 
+app.use(express.json()); // read JSON BODY
+app.use(express.urlencoded({ extended: true })); 
+
+app.post('/chatbot', (req, res) => {
+	res.header('Access-Control-Allow-Origin', '*');
+    const message = req.body.message;
+    const number = message.match(/\d+/);
+    if (number) {
+		fetch(`http://numbersapi.com/${number}?type=trivia`).then(response => response.text()).then(data => {
+			res.json({
+				text: data
+			});
+		}).catch(error => {
+			res.json({
+				text: "Sorry, I couldn't find any information about that number."
+			});
+		});
+	} else {
+        res.json({
+			text: "I'm sorry, I didn't understand your question. Please provide a number for me to give you information about."
+		});
+    }
+});
+
+
+const PORT = 3000;
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`);
+});
+
+// app.get('/', function(req, res) {
+//     res.sendFile(__dirname + "/index.html");
+// })
